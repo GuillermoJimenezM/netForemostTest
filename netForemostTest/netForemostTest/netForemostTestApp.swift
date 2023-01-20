be1be00b182837561acfab65f42b77bc81092a3f
Guillermo Jimenez <guillermo.jimenez@tribalworldwide.gt>
2023-01-19 19:36:43 -0600

    project creation

---
+++ netForemostTest/netForemostTest/netForemostTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  netForemostTestApp.swift
+//  netForemostTest
+//
+//  Created by Guillermo Jimenez on 19/1/23.
+//
+
+import SwiftUI
+
+@main
+struct netForemostTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
