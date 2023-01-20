21b8b10da78e295841c6f7fa894e53495b3f2eba
Guillermo Jimenez <guillermo.jimenez@tribalworldwide.gt>
2023-01-20 11:26:54 -0600

    Note view

---
+++ netForemostTest/Models/NoteModel.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  netForemostTest
+//
+//  Created by Guillermo Jimenez on 19/1/23.
+//
+
+import Foundation
+
+struct NoteModel {
+    
+    let id: UUID
+    var title: String
+    var body: String
+    var date: Date
+}
---
+++ netForemostTest/ViewModels/NoteViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  NoteViewModel.swift
+//  netForemostTest
+//
+//  Created by Guillermo Jimenez on 19/1/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreData
+
+@MainActor
+final class NoteViewModel: ObservableObject {
+    
+}
